6a68f1d09ebe0d5c4d34ccebbdf52f378651c1bc
Tkachenko Dima <dtkachenko@s-pro.io>
2024-12-20 14:57:57 +0200

    add config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "https://the-internet.herokuapp.com/",
+    retries: {
+      runMode: 2,
+      openMode: 2,
     },
+    video: true,
   },
 });
--- cypress/e2e/homeworks/homework17/herokuAppTests.cy.js
+++ cypress/e2e/homeworks/homework17/herokuAppTests.cy.js
@@ -1,17 +1,17 @@
 describe('HerokuApp Tests', () => {
     beforeEach(() => {
-        cy.visit('https://the-internet.herokuapp.com');
+        cy.visit( '/');
     });
 
     it('Verify A/B Testing page', () => {
         cy.contains('A/B Testing').click();
-        cy.url().should('include', '/abtest');
+        cy.url().should('include', 'abtest');
         cy.get('h3').should('contain', 'A/B Test');
     });
 
     it('Verify add/delete elements', () => {
         cy.contains('Add/Remove Elements').click();
-        cy.url().should('include', '/add_remove_elements/');
+        cy.url().should('include', 'add_remove_elements/');
         cy.contains('Add Element').click();
         cy.get('.added-manually').should('have.length', 1);
         cy.contains('Delete').click();
@@ -19,14 +19,14 @@ describe('HerokuApp Tests', () => {
 
     it('Verify checkboxes', () => {
         cy.contains('Checkboxes').click();
-        cy.url().should('include', '/checkboxes');
+        cy.url().should('include', 'checkboxes');
         cy.get('input[type="checkbox"]').first().check().should('be.checked');
         cy.get('input[type="checkbox"]').last().uncheck().should('not.be.checked');
     });
 
     it('Verify dropdown', () => {
         cy.contains('Dropdown').click();
-        cy.url().should('include', '/dropdown');
+        cy.url().should('include', 'dropdown');
         cy.get('#dropdown').select('Option 1').should('have.value', '1');
     });
 });
